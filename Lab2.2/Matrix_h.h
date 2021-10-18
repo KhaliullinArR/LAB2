551c6074fd5da292abb133f85f0dfce8c8ef2537
Khlaiullin Artem <halli-a@mail.ru>
2021-10-19 00:05:01 +0300

    m

---
+++ Lab2.2/Matrix_h.h
@@ -0,0 +1,12 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+
+double MatrixMultiply(double** , int const*, int const*);
+int PositiveElemsInRow(double* , int const*);
+int MatrixInput(double**& , int*, int*, const char*);
+int ResultsProcessing(double**, double**, int const*, int const*, int const*, int const*, double, double);
+
+#endif 
+
+
